afe5b5dc76fcdee84c1b59d0ce3d679775c2b168
unknown <leenmah4@gmail.com>
2023-11-17 12:53:39 +0200

    crud leen

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,13 @@ export default function App() {
   return (
  
     <Routes>
-   <Route path='/' />
+    <Route path='/' element={<Index/>}/>
   
    <Route path='/user/index' element={<Index/>} />
    <Route path='/user/create' element={<Create/>} />
    <Route path='/user/:id' element={<Details/>} />
    <Route path='/user/update/:id' element={<Update />} />
+   <Route path='*' element={<p>page not fouuuuuuuuuuuuuuuuuuunnd</p>}/>
    </Routes>
 
 
